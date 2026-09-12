910591bab44964c22995c3ae151ca38cd657f7f3
CarterReynosoBYUIStudent <rey21032@byui.edu>
2026-09-12 13:45:25 -0600

    HelloWorldFixed

--- HelloWorld/MyConsoleApp/Program.cs
+++ HelloWorld/MyConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿var currentTime = DateTime.Now;
-Console.WriteLine("Hello, World!");
-Console.WriteLine($"The current time is: {currentTime}");
-Console.WriteLine($"Days till Christmas: {(new DateTime(currentTime.Year, 12, 25) - currentTime).Days}");
+﻿var currentDate = DateTime.Now;
+Console.WriteLine($"Current date and time: {currentDate}");
+Console.WriteLine($"Days till Christmas: {(new DateTime(currentDate.Year, 12, 25) - currentDate).Days}");
+
