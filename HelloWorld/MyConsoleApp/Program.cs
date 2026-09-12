002698d3134a4b4dccde4c0ea5d139fdd75f6eb5
CarterReynosoBYUIStudent <rey21032@byui.edu>
2026-09-12 13:50:53 -0600

    test

--- HelloWorld/MyConsoleApp/Program.cs
+++ HelloWorld/MyConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿var currentDate = DateTime.Now;
 Console.WriteLine($"Current date and time: {currentDate}");
 Console.WriteLine($"Days till Christmas: {(new DateTime(currentDate.Year, 12, 25) - currentDate).Days}");
+Console.WriteLine("This is the first line. \nThis is the second line.");
 
