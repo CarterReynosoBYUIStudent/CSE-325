78c4c479b3a628a458f0c3bc0d3c679ad6eb5fac
CarterReynosoBYUIStudent <rey21032@byui.edu>
2026-09-26 09:39:17 -0600

    Week 2 chnages

--- HelloWorld/MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
+++ HelloWorld/MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704ea6da3644fadf7d5ba336ce71c01d6ddeec71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a2bce73e6514f413aace575f7d1bdc34deccf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
